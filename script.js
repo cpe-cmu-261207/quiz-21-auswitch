b1198d8a387dcfe200e52901e4e268eabbd625fe
auswitch <sawit_cha@cmu.ac.th>
2022-08-04 14:50:01 +0700

    "q2 completed"

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-async function callApi() {
+const Random = document.querySelector("#btn-random")
+const imgprofile = document.querySelector("#img-profile")
+const namee = document.querySelector("#p-name")
+const email = document.querySelector("#p-email")
+const address = document.querySelector("#p-address")
+const uc = document.querySelector("#div-user-card")
+
+
+
+async function callApi() 
+{
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+callApi();
+
+Random.onclick = () =>
+{
+  uc.style.display = "none";
+  namee.innerText = resp.data.name;
+};
